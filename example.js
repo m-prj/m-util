4a0755e55d4e18663ecfe8be344554cb2e8ec364
Misaki Shibata <misaki.pink@gmail.com>
2015-11-02 18:08:02 +0900

    no message

--- example.js
+++ example.js
@@ -7,3 +7,18 @@ $("*").css("border", "3px solid red");
 </script>
 */}).toString().uHereDoc();
 console.log(t);
+
+var path = 'test.png';
+console.log(path.uGetFileInfo());
+console.log(path.uGetFileNameExt());
+console.log(path.uGetFileExt());
+console.log(path.uGetFileName());
+console.log(path);
+
+
+var path2 = '/Users/mac/Documents/vhosts/m-util/index.js';
+console.log(path2.uGetFileInfo());
+console.log(path2.uGetFileNameExt());
+console.log(path2.uGetFileExt());
+console.log(path2.uGetFileName());
+console.log(path2);
--- index.js
+++ index.js
@@ -1,18 +1,51 @@
 module.exports = new(function() {
-  String.prototype.uHereDoc = function(){
+
+  
+  String.prototype.uHereDoc = function() {
     return this.replace(/^function\s?\(\)\s?\{\/\*/gi, "").replace(/\*\/;?\}$/gi, "");
   };
-  String.prototype.uAddslashes = function (s){
+  String.prototype.uAddslashes = function(s) {
     var reg = new RegExp(s, 'g');
-    return this.replace(reg, "\\"+ s);
+    return this.replace(reg, "\\" + s);
   }
-  String.prototype.uRepeat = function (i){
+  String.prototype.uRepeat = function(i) {
     var repeatStr = this;
     var str = "";
-    while(i>0){
+    while (i > 0) {
       str += repeatStr;
       i--;
     }
     return str;
+  }
+
+  /**
+   * パスからファイル情報返す
+   * @return ['ファイル名','拡張子','拡張子抜きファイル名']
+   */
+  String.prototype.uGetFileInfo = function() {
+      var file_path = this;
+      // Extract a file name with the extension.
+      var name_ext = file_path.substring(file_path.lastIndexOf("/") + 1, file_path.length);
+      // Extract only the extension of the file.
+      var ext = name_ext.substring(name_ext.lastIndexOf(".") + 1, name_ext.length);
+      // Extract only the name part of the file.
+      var name = name_ext.substring(0, name_ext.indexOf("."));
+      array = new Array(name_ext, ext, name);
+      return array;
+    }
+    // パスからファイル名を返す
+  String.prototype.uGetFileNameExt = function() {
+      file_path = this;
+      return file_path.uGetFileInfo()[0];
+    }
+    // パスから拡張子を返す
+  String.prototype.uGetFileExt = function() {
+      file_path = this;
+      return file_path.uGetFileInfo()[1];
+    }
+    // パスから拡張子抜きファイル名を返す
+  String.prototype.uGetFileName = function() {
+    file_path = this;
+    return file_path.uGetFileInfo()[2];
   }
 })();
